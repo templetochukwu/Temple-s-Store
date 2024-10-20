a143d0727f3e0996afbd7b97987cae148788dcc2
templetochukwu <templetochukwu99@gmail.com>
2024-10-20 13:53:40 +0100

    working on header

--- app/page.js
+++ app/page.js
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Button>Subscribe</Button>
+      Home
     </div>
   );
 }
